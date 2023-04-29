a3057fe667a27654737fdb31f9d6049ef784422f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-29 17:12:05 -0300

    feat: initializing basic project folder structure

---
+++ philo/includes/libs.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libs.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 16:55:03 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/04/29 16:56:02 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBS_H
+# define LIBS_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+#endif
--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -1,4 +1,7 @@
+#include "../includes/philosophers.h"
+
 int main(void)
 {
+	printf("Hi, hello Github\n");
 	return (0);
 }
