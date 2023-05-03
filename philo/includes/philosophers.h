2f6065ea3c1f2cf2b76db33493cdcd2b5b135ac9
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 14:23:44 -0300

    feat: ft_atol implemented

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,15 +6,16 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 14:17:52 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:21:01 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 # include "libs.h"
 
-int	ft_is_space(char c);
-int	ft_is_digit(char c);
-int	ft_is_sign(char c);
+int			ft_is_space(char c);
+int			ft_is_digit(char c);
+int			ft_is_sign(char c);
+long int	ft_atol(const char *nptr);
 
 #endif
---
+++ philo/srcs/aux_funcs/ft_atol.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 14:14:25 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/05/03 14:20:51 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/philosophers.h"
+
+long int	ft_atol(const char *nptr)
+{
+	int			i;
+	long int	res;
+	int			sign;
+
+	sign = 1;
+	i = 0;
+	res = 0;
+	while (ft_is_space(*(nptr + i)))
+		i++;
+	if (!ft_is_digit(*(nptr + i)) && !ft_is_sign(*(nptr + i)))
+		return (res);
+	if (ft_is_sign(*(nptr + i)))
+	{
+		if (*(nptr + i) == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ft_is_digit(*(nptr + i)))
+	{
+		res = ((res * 10) + (*(nptr + i) - '0') * sign);
+		i++;
+	}
+	return (res);
+}
