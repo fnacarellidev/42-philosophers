8aade2cd30a6d4d52d37588569deb296fc30a720
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 14:18:51 -0300

    feat: ft_is_sign implemented

--- philo/srcs/aux_funcs/ft_is_sign.c
+++ philo/srcs/aux_funcs/ft_is_sign.c
@@ -1,20 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   ft_is_sign.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 14:17:52 by fnacarel         ###   ########.fr       */
+/*   Created: 2023/05/03 14:16:05 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/05/03 14:17:37 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
-# include "libs.h"
+#include "../../includes/philosophers.h"
 
-int	ft_is_space(char c);
-int	ft_is_digit(char c);
-int	ft_is_sign(char c);
-
-#endif
+int	ft_is_sign(char c)
+{
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
+}
