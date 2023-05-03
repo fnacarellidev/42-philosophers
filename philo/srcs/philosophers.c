f4eeb52a8b26f34f703dc20222b213998d9911f8
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 14:28:24 -0300

    fix: previous srcs/philosophers.c was missing info
    some stuff has not been pushed yet and the previous version of this file had stuff that would depend on that stuff that was missing, this is a quick reset so I get back with the info before the functions, for example, it uses the struct table, but this struct has not been declared in the .h yet

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,55 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 13:55:33 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:26:40 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
-
-int mails = 0;
-pthread_mutex_t	mutex;
-
-void	routine(void)
-{
-	for (int i = 0; i < 100000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		mails++;
-		pthread_mutex_unlock(&mutex);
-	}
-}
-
-static void	init_philos(t_table *table)
-{
-	int			i;
-	pthread_t	*thread;
-	t_philo		*curr_philo;
-
-	i = 0;
-	table->philos = malloc(sizeof(t_philo) * table->nbr_of_philos);
-	while (i < table->nbr_of_philos)
-	{
-		curr_philo = table->philos + i;
-		curr_philo->id = i;
-		pthread_create(&(curr_philo->thread), NULL, routine, NULL);
-		printf("Thread %d just started\n", i);
-		i++;
-	}
-	i = 0;
-	while (i < table->nbr_of_philos)
-	{
-		curr_philo = table->philos + i;
-		pthread_join(curr_philo->thread, NULL);
-		printf("Thread %d just quit\n", i);
-		i++;
-	}
-}
-
-static void	init_table(t_table *table, char **argv)
-{
-	table->nbr_of_philos = atoi(argv[1]);
-	init_philos(table);
-}
 
 static void	print_error(int err_code)
 {
@@ -147,13 +102,7 @@ static int	valid_args(int params, char **args)
 
 int main(int argc, char **argv)
 {
-	t_table		table;
-
 	if (!valid_args(argc - 1, argv))
 		return (1);
-	init_table(&table, argv);
-	pthread_mutex_init(&mutex, NULL);
-	pthread_mutex_destroy(&mutex);
-	printf("Number of mails: %d\n", mails);
 	return (0);
 }
