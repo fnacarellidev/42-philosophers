dc09a2dc8ae2bc38f17476775e389abeffc8ae6a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 19:47:37 -0300

    refactor: no need to use init_philos_forks
    just initializing fork_left inside init_philos

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,28 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 18:52:17 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/03 19:46:28 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
 
 static void	init_philos(t_table *table)
-{
-	int		i;
-	t_philo	*curr_philo;
-
-	i = 0;
-	table->philo = malloc(sizeof(t_philo) * table->nbr_of_philos);
-	while (i < table->nbr_of_philos)
-	{
-		curr_philo = (table->philo) + i;
-		curr_philo->id = i;
-		pthread_mutex_init(&curr_philo->fork, NULL);
-		i++;
-	}
-}
-
-static void	init_philos_forks(t_table *table)
 {
 	int		i;
 	int		left_idx;
@@ -36,11 +20,14 @@ static void	init_philos_forks(t_table *table)
 
 	i = 0;
 	philos_qty = table->nbr_of_philos;
+	table->philo = malloc(sizeof(t_philo) * philos_qty);
 	while (i < philos_qty)
 	{
 		curr_philo = (table->philo) + i;
+		curr_philo->id = i;
 		left_idx = (i + philos_qty - 1) % philos_qty;
 		curr_philo->fork_left = &(table->philo[left_idx].fork);
+		pthread_mutex_init(&curr_philo->fork, NULL);
 		i++;
 	}
 }
@@ -49,7 +36,6 @@ static void	init_table(t_table *table, char **argv)
 {
 	table->nbr_of_philos = ft_atol(argv[1]);
 	init_philos(table);
-	init_philos_forks(table);
 }
 
 static void	die(t_table *table)
