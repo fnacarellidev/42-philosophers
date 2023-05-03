020634718764a7e32e75f98412e6476f9439835d
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 16:43:01 -0300

    refactor: moved error macros to separate header file

--- philo/includes/ft_errors.h
+++ philo/includes/ft_errors.h
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   ft_errors.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 16:41:48 by fnacarel         ###   ########.fr       */
+/*   Created: 2023/05/03 16:40:55 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/05/03 16:41:39 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
-# define PH_MAX_INT 2147483647
-# include "libs.h"
-# include "ft_errors.h"
-
-int			is_number(char *str);
-int			ft_is_space(char c);
-int			ft_is_digit(char c);
-int			ft_is_sign(char c);
-int			print_error(int err_code);
-int			valid_args(int params, char **args);
-long int	ft_atol(const char *nptr);
-
+#ifndef FT_ERRORS_H
+# define FT_ERRORS_H
+# define EPARAM 1
+# define ENOTNUMPARAM 2
+# define ELESSZERO 3
+# define EINTRANGE 4
+# define EPHILO 5
 #endif
