e04286f7bb767ac3e1789d991e2eff76d5b88da6
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-03 16:45:13 -0300

    style: just swapping order of declarations

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/03 16:41:48 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:43:54 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
@@ -15,10 +15,10 @@
 # include "libs.h"
 # include "ft_errors.h"
 
-int			is_number(char *str);
+int			ft_is_sign(char c);
 int			ft_is_space(char c);
 int			ft_is_digit(char c);
-int			ft_is_sign(char c);
+int			is_number(char *str);
 int			print_error(int err_code);
 int			valid_args(int params, char **args);
 long int	ft_atol(const char *nptr);
