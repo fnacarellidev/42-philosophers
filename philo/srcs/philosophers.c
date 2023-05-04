f4a78f136ed9f6cfa2fa2057adc47a0ac41d1c2c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-04 16:09:11 -0300

    feat: initializing philos and freeing them, no leaks

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,14 +6,30 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/04 16:03:46 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:07:24 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
 
+static void	init_philos(t_philo **philo_address, int philos_qty)
+{
+	*philo_address = malloc(sizeof(t_philo) * philos_qty);
+}
+
+static void	die(t_philo *philo)
+{
+	free(philo);
+}
+
 int	main(int argc, char **argv)
 {
+	int		philos_qty;
+	t_philo	*philo;
+
 	if (!valid_args(argc - 1, argv))
 		return (1);
+	philos_qty = ft_atol(argv[1]);
+	init_philos(&philo, philos_qty);
+	die(philo);
 	return (0);
 }
