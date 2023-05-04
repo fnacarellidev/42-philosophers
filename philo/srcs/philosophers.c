3450ebfccbd4638221c4bc3c36b7a715ab5db09b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-04 16:11:47 -0300

    feat: initializing each philo with it's id

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,14 +6,27 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/04 16:07:24 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:11:21 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
 
+static void	init_philos_ids(t_philo *philo, int philos_qty)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos_qty)
+	{
+		philo[i].id = i + 1;
+		i++;
+	}
+}
+
 static void	init_philos(t_philo **philo_address, int philos_qty)
 {
 	*philo_address = malloc(sizeof(t_philo) * philos_qty);
+	init_philos_ids(*philo_address, philos_qty);
 }
 
 static void	die(t_philo *philo)
