57cf3420627b1786ca67dc4a013d648a6203b5fb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-08 19:07:51 -0300

    feat: philos_watcher partially implemented
    this function is just a POC, it will must be refactored, but for now it seems to be doing what it's supposed to, it takes the difference of the current time stamp in ms with each philo last meal time stamp, if it is bigger than the time to die, it ends the simulation

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/08 17:23:22 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/08 18:54:58 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
@@ -25,6 +25,33 @@ static void	*routine(void *ptr)
 		philo->info.eat_many_times--;
 		if (philo->info.eat_many_times == 0)
 			break ;
+	}
+	return (NULL);
+}
+
+static void	*philos_watcher(void *info)
+{
+	int				i;
+	unsigned int	time_to_die;
+	t_philo			*curr_philo;
+	t_philo_info	*philo_info;
+
+	philo_info = (t_philo_info *)info;
+	while (1)
+	{
+		i = 0;
+		while (i < philo_info->philo_qty)
+		{
+			curr_philo = philo_info->philo + i;
+			time_to_die = curr_philo->info.time_to_die;
+			if (get_ms_timestamp() - curr_philo->last_meal >= time_to_die)
+			{
+				pthread_mutex_lock(philo_info->g_mut);
+				printf("%ld %d died\n", get_ms_timestamp() - curr_philo->ms_init_timestamp, curr_philo->id);
+				pthread_mutex_unlock(philo_info->g_mut);
+				exit(1);
+			}
+		}
 	}
 	return (NULL);
 }
