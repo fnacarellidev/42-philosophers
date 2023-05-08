73781c3ee6c5e2687a098723d4bd6319840165fc
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-08 13:00:47 -0300

    style: inserting tabs

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/08 12:27:15 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/08 12:44:35 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
@@ -33,14 +33,14 @@ typedef struct s_philo
 	pthread_mutex_t	*g_mut;
 }	t_philo;
 
-int			ft_is_sign(char c);
-int			ft_is_space(char c);
-int			ft_is_digit(char c);
-int			is_number(char *str);
-int			print_error(int err_code);
-int			valid_args(int params, char **args);
-long int	ft_atol(const char *nptr);
-void		init_philos(t_philo **ptr_address, int philos_qty, char **argv);
+int				ft_is_sign(char c);
+int				ft_is_space(char c);
+int				ft_is_digit(char c);
+int				is_number(char *str);
+int				print_error(int err_code);
+int				valid_args(int params, char **args);
+long int		ft_atol(const char *nptr);
+void			init_philos(t_philo **ptr_address, int philos_qty, char **argv);
 unsigned int	sec_to_milli(unsigned int seconds);
 unsigned int	milli_to_micro(unsigned int milliseconds);
 unsigned int	micro_to_milli(unsigned int microseconds);
