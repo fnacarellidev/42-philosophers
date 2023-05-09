6da6d3f7b4c503e673dcdaae3e9a4a2cf01adf83
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-09 16:51:54 -0300

    refactor: moving solve_n_philos to separate file

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:53:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/09 16:44:55 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:51:03 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
@@ -56,6 +56,7 @@ void			eat(t_philo *philo);
 void			rest(t_philo *philo);
 void			think(t_philo *philo);
 void			*philos_watcher(void *info);
+void			solve_n_philos(t_philo *philo, int philos_qty);
 time_t			sec_to_milli(time_t seconds);
 time_t			milli_to_micro(time_t milliseconds);
 time_t			micro_to_milli(time_t microseconds);
--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,57 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/09 16:48:23 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:51:39 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
-
-static void	*simulation(void *ptr)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *)ptr;
-	while (1)
-	{
-		take_forks(philo);
-		eat(philo);
-		if (!philo->stop_simulation)
-		{
-			rest(philo);
-			think(philo);
-		}
-		if (philo->info.eat_many_times == 0 || philo->stop_simulation)
-			return (NULL) ;
-	}
-	return (NULL);
-}
-
-static void	solve_n_philos(t_philo *philo, int philos_qty)
-{
-	int				i;
-	t_philo			*curr_philo;
-	t_philo_info	philo_info;
-
-	i = 0;
-	philo_info.philo = philo;
-	philo_info.philo_qty = philos_qty;
-	while (i < philos_qty)
-	{
-		curr_philo = philo + i;
-		pthread_create(&curr_philo->thread, NULL, &simulation, (void *)curr_philo);
-		i++;
-	}
-	philo_info.g_mut = philo[0].g_mut;
-	pthread_create(&philo_info.thread, NULL, &philos_watcher, (void *)&philo_info);
-	i = 0;
-	while (i < philos_qty)
-	{
-		curr_philo = philo + i;
-		pthread_join(curr_philo->thread, NULL);
-		i++;
-	}
-	pthread_join(philo_info.thread, NULL);
-}
 
 static void	die(t_philo *philo, int philos_qty)
 {
