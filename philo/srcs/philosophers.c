47c58783bb50738e727f97b035d7482422113780
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-09 16:49:34 -0300

    refactor: renaming functions
    init_threads renamed to solve_n_philos and routine renamed to simulation

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,12 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:55:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/09 16:36:21 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:48:23 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers.h"
 
-static void	*routine(void *ptr)
+static void	*simulation(void *ptr)
 {
 	t_philo	*philo;
 
@@ -75,7 +75,7 @@ static void	*philos_watcher(void *info)
 	return (NULL);
 }
 
-static void	init_threads(t_philo *philo, int philos_qty)
+static void	solve_n_philos(t_philo *philo, int philos_qty)
 {
 	int				i;
 	t_philo			*curr_philo;
@@ -87,7 +87,7 @@ static void	init_threads(t_philo *philo, int philos_qty)
 	while (i < philos_qty)
 	{
 		curr_philo = philo + i;
-		pthread_create(&curr_philo->thread, NULL, &routine, (void *)curr_philo);
+		pthread_create(&curr_philo->thread, NULL, &simulation, (void *)curr_philo);
 		i++;
 	}
 	philo_info.g_mut = philo[0].g_mut;
@@ -124,7 +124,7 @@ int	main(int argc, char **argv)
 		return (1);
 	philos_qty = ft_atol(argv[1]);
 	init_philos(&philo, philos_qty, argv);
-	init_threads(philo, philos_qty);
+	solve_n_philos(philo, philos_qty);
 	die(philo, philos_qty);
 	return (0);
 }
