25537e9cac8068176235e07ef6b01d70fc7ca947
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-10 20:23:00 -0300

    feat: (bonus) ft_defines header set

--- philo_bonus/includes/ft_defines.h
+++ philo_bonus/includes/ft_defines.h
@@ -1,16 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers_bonus.h                               :+:      :+:    :+:   */
+/*   ft_defines.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/10 11:02:11 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/10 20:14:20 by fnacarel         ###   ########.fr       */
+/*   Created: 2023/05/10 20:09:50 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/05/10 20:10:15 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef PHILOSOPHERS_BONUS_H
-# define PHILOSOPHERS_BONUS_H
-# include "libs_bonus.h"
-# include "ft_defines.h"
+#ifndef FT_DEFINES_H
+# define FT_DEFINES_H
+# define PH_MAX_INT 2147483647
+# define EPARAM 1
+# define ENOTNUMPARAM 2
+# define ELESSZERO 3
+# define EINTRANGE 4
+# define EPHILO 5
 #endif
