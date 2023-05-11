2cb8478dca24c581c6ec6dda5a04d7f040c81158
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-11 18:37:07 -0300

    feat: (bonus) lots of stuff
    a lot of code commited but not much going on, initializing philo info (id, last_meal, time_to_die, time_to_eat, time_to_sleep, number of meals and getting initial timestamp of program

--- philo_bonus/includes/philosophers_bonus.h
+++ philo_bonus/includes/philosophers_bonus.h
@@ -6,13 +6,37 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:02:11 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/11 12:23:33 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/11 18:27:23 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_BONUS_H
 # define PHILOSOPHERS_BONUS_H
 # include "libs_bonus.h"
 # include "ft_defines.h"
+
+typedef struct s_timer
+{
+	time_t	time_to_die;
+	time_t	time_to_eat;
+	time_t	time_to_sleep;
+	time_t	last_meal;
+}	t_timer;
+
+typedef struct s_philo
+{
+	size_t	id;
+	pid_t	pid;
+	int		meals;
+	t_timer	timers;
+}	t_philo;
+
+typedef struct s_data
+{
+	time_t	ms_init;
+	size_t	*pid;
+	size_t	curr_philo;
+	t_philo	*philos;
+}	t_data;
 
 long int	ft_atol(const char *nptr);
 int			ft_is_digit(char c);
@@ -25,5 +49,4 @@ time_t		get_ms_timestamp(void);
 time_t		micro_to_milli(time_t microseconds);
 time_t		sec_to_milli(time_t seconds);
 time_t		milli_to_micro(time_t milliseconds);
-
 #endif
--- philo_bonus/srcs/philosophers_bonus.c
+++ philo_bonus/srcs/philosophers_bonus.c
@@ -6,14 +6,70 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 20:14:32 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/10 20:14:33 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/11 18:26:31 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers_bonus.h"
 
-int main(int argc, char **argv)
+static void	init_philos(t_data *data, size_t philos_qty, char **argv)
 {
+	size_t	i;
+	time_t	init_meal;
+
+	i = 0;
+	while (i < philos_qty)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].timers.time_to_die = ft_atol(argv[2]);
+		data->philos[i].timers.time_to_eat = ft_atol(argv[3]);
+		data->philos[i].timers.time_to_sleep = ft_atol(argv[4]);
+		if (argv[5])
+			data->philos[i].meals = ft_atol(argv[5]);
+		else
+			data->philos[i].meals = -1;
+		i++;
+	}
+	init_meal = get_ms_timestamp();
+	i = 0;
+	while (i < philos_qty)
+	{
+		data->philos[i].timers.last_meal = init_meal;
+		i++;
+	}
+}
+
+void	init_data(t_data *data, size_t philos_qty, char **argv)
+{
+	size_t	i;
+
+	i = 0;
+	data->ms_init = get_ms_timestamp();
+	data->philos = malloc(sizeof(t_philo) * philos_qty);
+	init_philos(data, philos_qty, argv);
+	while (i < philos_qty)
+	{
+		data->curr_philo = i;
+		i++;
+	}
+}
+
+static void	die(t_data *data)
+{
+	free(data);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	*data;
+	size_t	philos_qty;
+
 	if (!valid_args(argc - 1, argv))
 		return (1);
+	data = malloc(sizeof(t_data));
+	if (data == NULL)
+		return (1);
+	philos_qty = (size_t)ft_atol(argv[1]);
+	init_data(data, philos_qty, argv);
+	die(data);
 	return (0);
 }
