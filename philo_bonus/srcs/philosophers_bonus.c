0f2ff2833f0a71c0353f7ca719d7a7c196198c87
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-13 15:13:03 -0300

    fix: (bonus) was not closing semaphore at end of program

--- philo_bonus/srcs/philosophers_bonus.c
+++ philo_bonus/srcs/philosophers_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 20:14:32 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/12 19:57:27 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/13 15:11:04 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers_bonus.h"
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_data	*data;
 	size_t	philos_qty;
 
-	i = 0;
+	i = -1;
 	if (!valid_args(argc - 1, argv))
 		return (1);
 	data = malloc(sizeof(t_data));
@@ -25,17 +25,15 @@ int	main(int argc, char **argv)
 		return (1);
 	philos_qty = (size_t)ft_atol(argv[1]);
 	init_data(data, philos_qty, argv);
-	while (i < philos_qty)
-	{
-		wait(NULL);
-		i++;
-	}
+	while (++i < philos_qty)
+		waitpid(-1, NULL, 0);
 	die(data);
 	return (0);
 }
 
 void	die(t_data *data)
 {
+	sem_close(data->forks);
 	sem_unlink("/sem_forks");
 	free(data->pid);
 	free(data->philos);
