ae7801eaec74b3360350c07f26e109ae5430c151
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-16 17:48:18 -0300

    refactor: refactoring actions, kill_philo will print the message too

--- philo_bonus/srcs/philo_actions_bonus.c
+++ philo_bonus/srcs/philo_actions_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 14:02:55 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/16 17:35:53 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:41:59 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philosophers_bonus.h"
@@ -30,12 +30,24 @@ void	print_action(char *suffix, time_t init_ms, t_philo philo)
 		printf("%ld\t%ld has taken a fork\n", time_elapsed, philo.id);
 }
 
-void	kill_philo(t_data *data, unsigned int exit_code)
+void	kill_philo(t_data *data, t_philo *philo)
 {
+	struct timeval	tv;
+	time_t			curr_ms;
+	time_t			death_timestamp;
+
+	gettimeofday(&tv, NULL);
+	curr_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	death_timestamp = philo->timers.last_meal + philo->timers.time_to_die;
+	usleep((death_timestamp - curr_ms) * 1000);
+	sem_wait(data->print_sem);
+	gettimeofday(&tv, NULL);
+	curr_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	printf("%ld\t%ld died\n", curr_ms - data->ms_init, philo->id);
 	sem_close(data->forks);
 	sem_close(data->print_sem);
 	die(data);
-	exit(exit_code);
+	exit(1);
 }
 
 void	take_forks(t_data *data, t_philo *philo)
@@ -65,14 +77,7 @@ void	eat(t_data *data, t_philo *philo)
 	action_timestamp = curr_ms + philo->timers.time_to_eat;
 	print_action("eating", data->ms_init, *philo);
 	if (action_timestamp > death_timestamp)
-	{
-		usleep((death_timestamp - curr_ms) * 1000);
-		sem_wait(data->print_sem);
-		gettimeofday(&tv, NULL);
-		curr_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-		printf("%ld\t%ld died\n", curr_ms - data->ms_init, philo->id);
-		kill_philo(data, 1);
-	}
+		kill_philo(data, philo);
 	usleep(philo->timers.time_to_eat * 1000);
 	gettimeofday(&tv, NULL);
 	curr_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
@@ -92,13 +97,6 @@ void	rest(t_data *data, t_philo *philo)
 	action_timestamp = curr_ms + philo->timers.time_to_eat;
 	print_action("sleeping", data->ms_init, *philo);
 	if (action_timestamp > death_timestamp)
-	{
-		usleep((death_timestamp - curr_ms) * 1000);
-		sem_wait(data->print_sem);
-		gettimeofday(&tv, NULL);
-		curr_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-		printf("%ld\t%ld died\n", curr_ms - data->ms_init, philo->id);
-		kill_philo(data, 1);
-	}
+		kill_philo(data, philo);
 	usleep(philo->timers.time_to_eat * 1000);
 }
