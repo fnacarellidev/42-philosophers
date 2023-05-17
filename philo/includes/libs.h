de36564d4a3ee54ee07bb029685902f619191430
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-17 14:33:27 -0300

    refactor: removing unused libs

--- philo/includes/libs.h
+++ philo/includes/libs.h
@@ -6,12 +6,11 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:55:03 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/29 16:56:02 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/17 14:30:29 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBS_H
 # define LIBS_H
-# include <string.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
--- philo_bonus/includes/libs_bonus.h
+++ philo_bonus/includes/libs_bonus.h
@@ -6,17 +6,15 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:27:00 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/05/11 12:37:54 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/05/17 14:32:38 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBS_BONUS_H
 # define LIBS_BONUS_H
-# include <string.h>
-# include <unistd.h>
 # include <stdio.h>
+# include <unistd.h>
 # include <stdlib.h>
 # include <signal.h>
-# include <pthread.h>
 # include <sys/time.h>
 # include <sys/wait.h>
 # include <fcntl.h>
